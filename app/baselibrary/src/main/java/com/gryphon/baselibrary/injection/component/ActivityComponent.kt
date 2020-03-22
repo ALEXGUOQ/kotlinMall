34ad2f6dae2c30c6c9fac60f8f41565c12bf62f1
gqiyuan <gqiyuan@linewell.com>
2020-03-23 07:20:42 +0800

    添加方法

--- app/baselibrary/src/main/java/com/gryphon/baselibrary/injection/component/ActivityComponent.kt
+++ app/baselibrary/src/main/java/com/gryphon/baselibrary/injection/component/ActivityComponent.kt
@@ -1,9 +1,11 @@
 package com.gryphon.baselibrary.injection.component
 
 import android.app.Activity
+import android.content.Context
 import com.gryphon.baselibrary.injection.ActivityScope
 import com.gryphon.baselibrary.injection.module.ActivityModule
 import com.gryphon.baselibrary.injection.module.LifeCycleProviderModule
+import com.trello.rxlifecycle3.LifecycleProvider
 import dagger.Component
 
 /**
@@ -13,7 +15,9 @@ import dagger.Component
  * @date: 2020/3/21 11:38
  */
 @ActivityScope
-@Component(dependencies = [AppComponent::class], modules = [ActivityModule::class,LifeCycleProviderModule::class])
+@Component(dependencies = [AppComponent::class], modules = [ActivityModule::class,LifeCycleProviderModule::class ])
 interface ActivityComponent {
     fun activity():Activity
+    fun context():Context
+    fun lifeCycleProvider():LifecycleProvider<*>
 }
